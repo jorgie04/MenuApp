f6558c561e3c2d3092cbca3a741ed414d33cee78
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2023-05-19 03:53:45 +0800

    Firebase connection and some fixes

--- app/src/main/java/com/ceit/menuapp/MainActivity.java
+++ app/src/main/java/com/ceit/menuapp/MainActivity.java
@@ -1,21 +1,53 @@
 package com.ceit.menuapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+//import android.support.annotation.NonNull;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
 
      ImageButton buttonToOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> users =  new HashMap<>();
+        users.put("description", "JOKER");
+        users.put("firstname", "NIYO");
+        users.put("idk", "PAGOD");
+        users.put("rawr", "NA");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
+            }
+        });
 
         // To hide action bar
         if (getSupportActionBar() != null) {
