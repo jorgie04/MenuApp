725df1d164c89420b72f8444403cee6a00db0fe5
jorgie04 <116165767+jorgie04@users.noreply.github.com>
2023-05-18 21:25:04 +0800

    patry idol
    error sakin yung page curl, patest kung ayaw din.

--- app/src/main/java/com/ceit/menuapp/order_Screen.java
+++ app/src/main/java/com/ceit/menuapp/order_Screen.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.num.numandroidpagecurleffect.PageCurlView;
 
 public class order_Screen extends AppCompatActivity {
 
     ImageButton drinks;
     ImageButton checkOut;
+    PageCurlView pageCurlView;
+    List<Integer> dishes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +27,18 @@ public class order_Screen extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        pageCurlView = findViewById(R.id.pagecurlView);
+        dishes= new ArrayList<>();
+
+        dishes.add(R.drawable.dish_one); // First Dish
+        dishes.add(R.drawable.dish_two); // Second Dish
+        dishes.add(R.drawable.dish_three); // Third Dish
+
+        pageCurlView.setCurlView(dishes);
+        pageCurlView.setCurlSpeed(600);
+
         // To go to Drinks Screen
-        drinks = findViewById(R.id.drinks);
+        drinks = findViewById(R.id.drinks_button);
         drinks.setOnClickListener(view -> {
 
             Intent intent=new Intent(order_Screen.this,drinks_screen.class);
@@ -30,7 +46,7 @@ public class order_Screen extends AppCompatActivity {
         });
 
         // To go to Check out Screen
-        checkOut = findViewById(R.id.checkOut);
+        checkOut = findViewById(R.id.checkout);
         checkOut.setOnClickListener(view -> {
 
             Intent intent=new Intent(order_Screen.this, check_out.class);
